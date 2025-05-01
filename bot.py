009f480fd35deb9da7b08826c20f59a046ecbbd1
info-zone <testaccountbsxfsf@protonmail.com>
2025-05-01 11:00:31 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,7 +8,7 @@ from openai import OpenAI
 BOT_TOKEN = "2109246071:LvlHCpvSkjpD8rFw1N4lNcaJmKP5EyCxgUNp6euX"
 HF_TOKEN = "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"
 HF_API_URL = "https://router.huggingface.co/hf-inference/models/black-forest-labs/FLUX.1-dev"
-OR_API_KEY = "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"
+OR_API_KEY = "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"
 SYSTEM_PROMPT = "شما یک دستیار فارسی زبان هستید. مودب، مفید و خلاصه جواب بده. در صورتی که سوال مربوط به تصویر بود از کاربر بخواه که از دستور /gen استفاده کند."
 SPAM_DELAY = 30  # seconds between /gen requests
 URL = f"https://tapi.bale.ai/bot{BOT_TOKEN}/"
